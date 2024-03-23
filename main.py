8902ac861add5136b711feed904866952ee07f76
Nima <100118467+NimaWasTaken@users.noreply.github.com>
2024-03-23 21:14:54 +0330

    Refactored the code

--- main.py
+++ main.py
@@ -70,8 +70,11 @@ def send_request(api_name, api_url, data, timeout, proxy=None):
 
         return f"{Fore.YELLOW}[{current_time}] {Fore.GREEN}[+] {api_name} =>{Style.RESET_ALL} OK"
     except requests.exceptions.RequestException as e:
-        error_code = response.status_code if response else "Unknown"
-        return f"{Fore.YELLOW}[{current_time}] {Fore.RED}[-] {api_name} =>{Style.RESET_ALL} Error {error_code}"
+        if response:
+            error_code = response.status_code
+        else:
+            error_code = "Unknown"
+        return f"{Fore.YELLOW}[{current_time}] {Fore.RED}[-] {api_name} =>{Style.RESET_ALL} Error {error_code}: {e}"
 
 
 def process_target(api, proxy):
